a495ede1d699a0a239871feb0528d5d88e49a06e
Musa <17muab4044@gmail.com>
2026-01-24 23:59:45 +0000

    k

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ st.metric("Average Transaction", f"£{avg_transaction:,.2f}")
 
 st.divider()
 
-#visualisations
+#visualisationshhupu
 st.markdown("---")
 st.header("Visualisations Overview")
 st.subheader("Spending by category")
